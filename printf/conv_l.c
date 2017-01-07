8c6ac787fdbc7ac99a4d570cc2c8987506e87e16
Antonin RIDOLFI <aridolfi@student.42.fr>
2017-01-07 15:09:39 +0100

    Add fix pointer error

--- printf/conv_l.c
+++ printf/conv_l.c
@@ -6,39 +6,36 @@
 /*   By: aridolfi <aridolfi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/05 15:58:49 by aridolfi          #+#    #+#             */
-/*   Updated: 2017/01/07 14:22:34 by aridolfi         ###   ########.fr       */
+/*   Updated: 2017/01/07 15:09:10 by aridolfi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-char	*ft_wchar_to_str(wchar_t c)
+void	ft_wchar_to_str(wchar_t c, char **s)
 {
-	char *s;
-
-	s = ft_strnew(4);
+	*s = ft_strnew(4);
 	check_alloc((void*)s);
 	if (c <= 0x7F)
-		s[0] = c;
+		(*s)[0] = c;
 	else if (c < 0x7FF)
 	{
-		s[0] = (c >> 6) + 0xC0;
-		s[1] = (c & 0x3F) + 0x80;
+		(*s)[0] = (c >> 6) + 0xC0;
+		(*s)[1] = (c & 0x3F) + 0x80;
 	}
 	else if (c < 0xFFFF)
 	{
-		s[0] = (c >> 12) + 0xE0;
-		s[1] = ((c >> 6) & 0x3F) + 0x80;
-		s[2] = (c & 0x3F) + 0x80;
+		(*s)[0] = (c >> 12) + 0xE0;
+		(*s)[1] = ((c >> 6) & 0x3F) + 0x80;
+		(*s)[2] = (c & 0x3F) + 0x80;
 	}
 	else if (c <= 0x10FFFF)
 	{
-		s[0] = (c >> 18) + 0xF0;
-		s[1] = (c >> 12) & 0x3F;
-		s[2] = ((c >> 6) & 0x3F) + 0x80;
-		s[3] = (c & 0x3F) + 0x80;
+		(*s)[0] = (c >> 18) + 0xF0;
+		(*s)[1] = (c >> 12) & 0x3F;
+		(*s)[2] = ((c >> 6) & 0x3F) + 0x80;
+		(*s)[3] = (c & 0x3F) + 0x80;
 	}
-	return (s);
 }
 
 char	*ft_conv_s(char *buff, va_list ap, t_opt form_arg)
@@ -59,6 +56,7 @@ char	*ft_conv_ws(char *buff, va_list ap, t_opt form_arg)
 {
 	wchar_t	*wstr;
 	char	*tmp;
+	char	*s;
 
 	if (form_arg.conv)
 	{
@@ -66,9 +64,10 @@ char	*ft_conv_ws(char *buff, va_list ap, t_opt form_arg)
 		while (*wstr != 0)
 		{
 			tmp = buff;
-			buff = ft_strjoin(buff, ft_wchar_to_str(*wstr));
+			buff = ft_strjoin(buff, ft_wchar_to_str(*wstr, &s));
 			check_alloc((void *)buff);
 			free(tmp);
+			free(s);
 		}
 	}
 	return (buff);
@@ -96,13 +95,15 @@ char	*ft_conv_c(char *buff, va_list ap, t_opt form_arg)
 char	*ft_conv_wc(char *buff, va_list ap, t_opt form_arg)
 {
 	char	*tmp;
+	char	*s;
 
 	if (form_arg.conv)
 	{
 		tmp = buff;
-		buff = ft_strjoin(buff, ft_wchar_to_str(va_arg(ap, wchar_t)));
+		buff = ft_strjoin(buff, ft_wchar_to_str(va_arg(ap, wchar_t), &s));
 		check_alloc((void *)buff);
 		free(tmp);
+		free(s);
 	}
 	return (buff);
 }
