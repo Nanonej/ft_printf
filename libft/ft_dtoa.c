fd5549a0503364d2981c952ceea1418e764e25aa
Vertylo <lchim@student.42.fr>
2017-01-11 11:27:42 +0100

    Add ft_dtoa.c

---
+++ libft/ft_dtoa.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_dtoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aridolfi <aridolfi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/14 14:10:29 by aridolfi          #+#    #+#             */
+/*   Updated: 2017/01/11 11:19:22 by lchim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char		*dtoa(double c, char sign)
+{
+	int		i;
+	int		tmp;
+	char	*ent;
+	char	*ret;
+
+	i = (int)c;
+	c -= (double)i;
+	while (c != (double)(tmp = (int) c))
+		c *= 10;
+	ent = ft_strjoin(ft_itoa_base(i, 10, sign), ".");
+	if (ent == NULL)
+		return (NULL);
+	ret = ft_strjoin(ent, ft_itoa(c));
+	if (ret == NULL)
+		return (NULL);
+	free(ent);
+	return (ret);
+}
